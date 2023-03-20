e09434ec35eddf02d56116f959d5db248c6ad87e
nguyenthtruc <ngtruc1210@gmail.com>
2023-03-20 18:32:03 +0700

    Xay dung layouts

---
+++ src/component/layouts/authRoute.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Fragment } from "react";
+import { Redirect, Route } from "react-router-dom";
+import Toast from "../home/toast";
+import { toast, ToastContainer } from "react-toastify";
+
+class AuthRoute extends React.Component {
+
+  componentDidMount = () => {
+    // console.log(this.isActive !== false)
+    if(this.isActive === false){
+      toast.warning(<Toast message="Đăng nhập để tiếp tục"/>,{
+          className: "warning",
+          // autoClose: false,
+        })
+    }
+  };
+
+  render() {
+    this.isActive = JSON.parse(localStorage.getItem("isactive"));
+    return (
+      <Fragment>
+        {this.isActive !== false ? (
+          <Route component={this.props.component}></Route>
+        ) : (
+          <Fragment>
+            <ToastContainer />
+            <Redirect to="/login"></Redirect>
+          </Fragment>
+        )}
+      </Fragment>
+    );
+  }
+}
+
+export default AuthRoute;
